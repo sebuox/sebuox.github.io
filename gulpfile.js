1e96ae5d8a312a1cbd8cca711c70dacc4d4f2f03
Seb <129689410+sebuox@users.noreply.github.com>
2023-04-16 21:26:57 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('scripts', function() {
 gulp.task('watch',function() {
     gulp.watch('wp-content/themes/themename/assets/sass/*.scss',['styles']);
     gulp.watch('wp-content/themes/themename/assets/js/*.js',['scripts']);
-});
+
 exports.default = series(scssTask, jsTask, browserSysnServe, watchTask);
 
 exports.build ) = series(scssTask, jsTask);
