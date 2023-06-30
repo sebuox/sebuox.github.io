49a0199c7800521474d200d94d8bbedad50dddbc
Seb <129689410+sebuox@users.noreply.github.com>
2023-06-30 10:25:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Define routes
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+c
