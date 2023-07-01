bd47ecd9ab2e30bcc16ac9a418bd9fd308d9b0d4
Seb <129689410+sebuox@users.noreply.github.com>
2023-07-01 17:56:56 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const clientId = 'rsbdo-usvu5hwsIqvlCifA';
+const apiKey = 'NQsM2CYKBClp6Otfe-7MeOTLAiR4rY-d3XfYNiDFnI7GqCAzAqTgu-CJCh-ahP7CSka-xPzwJqMovSsx-nY-NLgJraeqZjRnDWsWBmhVMNXtGbGDqFXZiRPFjUOgZHYx';
+
 // Retrieve user's location using the Geolocation API
 function getLocation() {
   if (navigator.geolocation) {
@@ -11,45 +14,47 @@ function getLocation() {
 function showPosition(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  
+
   // Make API request to Yelp with user's location coordinates
   const yelpAPIUrl = `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&term=food`;
-  
+
   // Use the retrieved API URL to fetch data from Yelp
   fetch(yelpAPIUrl, {
     headers: {
-      "Authorization": "Bearer NQsM2CYKBClp6Otfe-7MeOTLAiR4rY-d3XfYNiDFnI7GqCAzAqTgu-CJCh-ahP7CSka-xPzwJqMovSsx-nY-NLgJraeqZjRnDWsWBmhVMNXtGbGDqFXZiRPFjUOgZHYx" // Replace with your Yelp API key
+      'Authorization': `Bearer ${apiKey}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
   })
-  .then(response => response.json())
-  .then(data => displayResults(data.businesses))
-  .catch(error => console.error(error));
+    .then(response => response.json())
+    .then(data => displayResults(data.businesses))
+    .catch(error => console.error(error));
 }
 
 // Function to display the search results on the page
 function displayResults(restaurants) {
-  const resultsContainer = document.getElementById("results-container");
-  resultsContainer.innerHTML = ""; // Clear previous results
-  
+  const resultsContainer = document.getElementById('results-container');
+  resultsContainer.innerHTML = ''; // Clear previous results
+
   restaurants.forEach(restaurant => {
     const name = restaurant.name;
     const rating = restaurant.rating;
     const image = restaurant.image_url;
-    
+
     // Create HTML elements to display the restaurant information
-    const restaurantElement = document.createElement("div");
-    const nameElement = document.createElement("h2");
-    const ratingElement = document.createElement("p");
-    const imageElement = document.createElement("img");
-    
+    const restaurantElement = document.createElement('div');
+    const nameElement = document.createElement('h2');
+    const ratingElement = document.createElement('p');
+    const imageElement = document.createElement('img');
+
     nameElement.innerText = name;
     ratingElement.innerText = `Rating: ${rating}`;
     imageElement.src = image;
-    
+
     restaurantElement.appendChild(nameElement);
     restaurantElement.appendChild(ratingElement);
     restaurantElement.appendChild(imageElement);
-    
+
     resultsContainer.appendChild(restaurantElement);
   });
 }
@@ -58,4 +63,3 @@ function displayResults(restaurants) {
 function searchRestaurants() {
   getLocation();
 }
-
