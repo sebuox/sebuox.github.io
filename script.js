2ad21b266de270fc8750d8154ea30fe85fa25a61
Seb <129689410+sebuox@users.noreply.github.com>
2023-07-01 17:52:06 +0200

    Update and rename scr to script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+// Retrieve user's location using the Geolocation API
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+// Callback function to display user's location
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  
+  // Make API request to Yelp with user's location coordinates
+  const yelpAPIUrl = `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&term=food`;
+  
+  // Use the retrieved API URL to fetch data from Yelp
+  fetch(yelpAPIUrl, {
+    headers: {
+      "Authorization": "Bearer YOUR_YELP_API_KEY" // Replace with your Yelp API key
+    }
+  })
+  .then(response => response.json())
+  .then(data => displayResults(data.businesses))
+  .catch(error => console.error(error));
+}
+
+// Function to display the search results on the page
+function displayResults(restaurants) {
+  const resultsContainer = document.getElementById("results-container");
+  resultsContainer.innerHTML = ""; // Clear previous results
+  
+  restaurants.forEach(restaurant => {
+    const name = restaurant.name;
+    const rating = restaurant.rating;
+    const image = restaurant.image_url;
+    
+    // Create HTML elements to display the restaurant information
+    const restaurantElement = document.createElement("div");
+    const nameElement = document.createElement("h2");
+    const ratingElement = document.createElement("p");
+    const imageElement = document.createElement("img");
+    
+    nameElement.innerText = name;
+    ratingElement.innerText = `Rating: ${rating}`;
+    imageElement.src = image;
+    
+    restaurantElement.appendChild(nameElement);
+    restaurantElement.appendChild(ratingElement);
+    restaurantElement.appendChild(imageElement);
+    
+    resultsContainer.appendChild(restaurantElement);
+  });
+}
+
+// Function to initiate the search with user's location
+function searchRestaurants() {
+  getLocation();
+}
+
